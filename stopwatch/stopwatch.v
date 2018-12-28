75ce274b94661fa6af3d573dc86090e9100d0fb8
Clifford Wolf <clifford@clifford.at>
2018-12-28 19:23:53 +0100

    Progress in initial stopwatch verilog
    Signed-off-by: Clifford Wolf <clifford@clifford.at>

--- stopwatch/stopwatch.v
+++ stopwatch/stopwatch.v
@@ -18,9 +18,9 @@ module top (
 	reg clkdiv_pulse = 0;
 
 	assign LED1 = !BTN_N;
-	assign LED2 = BTN1;
-	assign LED3 = BTN2;
-	assign LED4 = BTN3;
+	assign LED2 = BTN1 || BTN2;
+	assign LED3 = BTN2 ^ BTN3;
+	assign LED4 = BTN3 && !BTN_N;
 	assign LED5 = (BTN1 + BTN2 + BTN3 + 2'b00) >> 1;
 
 	always @(posedge CLK) begin
@@ -120,12 +120,12 @@ module seven_seg_hex (
 			4'h0: dout = 7'b 0111111;
 			4'h1: dout = 7'b 0000110;
 			4'h2: dout = 7'b 1011011;
-			4'h3: dout = 7'b 1001111;
+			// 4'h3: dout = FIXME;
 			4'h4: dout = 7'b 1100110;
 			4'h5: dout = 7'b 1101101;
 			4'h6: dout = 7'b 1111101;
 			4'h7: dout = 7'b 0000111;
-			4'h8: dout = 7'b 1111111;
+			// 4'h8: dout = FIXME;
 			4'h9: dout = 7'b 1101111;
 			4'hA: dout = 7'b 1110111;
 			4'hB: dout = 7'b 1111100;
