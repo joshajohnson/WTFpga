b829aa8cd25bf99d23a56ab349301b2e17b0964d
Josh Johnson <josh@joshajohnson.com>
2019-09-03 13:36:05 +1000

    now builds and more typo fix

--- wtfpga-lab/displaySelect.v
+++ wtfpga-lab/displaySelect.v
@@ -1,31 +1,31 @@
-`default_nettype none
+// `default_nettype none
+//
+// module displaySelect(
+//     input clk,
+//     input [7:0] sw,
+//     input switch, 
+//     output reg [3:0] nibbleMS,
+//     output reg [3:0] nibbleLS
+//     );
 
-module displaySelect(
-    input clk,
-    input [7:0] sw,
-    input switch, 
-    output reg [3:0] nibbleMS,
-    output reg [3:0] nibbleLS
-    );
+// 	// add dispNum here
 
-	// add dispNum here
+//     // change between dec and hex display
+//     always @() begin
+//         // if in hex mode, pass switches through
+//         if () begin
 
-    // change between dec and hex display
-    always @() begin
-        // if in hex mode, pass switches through
-        if () begin
-
-        end else begin
-            // determine value to display in most significant display
-            // if (sw >= 7'd90) begin
-            //     nibbleMS = 4'd9;
-            // end else if (sw >= 8'd80) begin
-            //     nibbleMS = 4'd8;
-            // end else if (sw >= 8'd70) begin
-            //     nibbleMS = 4'd7;
-            // end
-            // // calculate least significant digit
-            // nibbleLS =  -  * ;
-        end
-    end
-endmodule
+//         end else begin
+//             // determine value to display in most significant display
+//             // if (sw >= 7'd90) begin
+//             //     nibbleMS = 4'd9;
+//             // end else if (sw >= 8'd80) begin
+//             //     nibbleMS = 4'd8;
+//             // end else if (sw >= 8'd70) begin
+//             //     nibbleMS = 4'd7;
+//             // end
+//             // // calculate least significant digit
+//             // nibbleLS =  -  * ;
+//         end
+//     end
+// endmodule
